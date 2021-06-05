036cb5ed6e8a7c17ceddc9d716c7c858c541104a
sanua356 <51795446+sanua356@users.noreply.github.com>
2021-06-05 20:59:28 +0300

    Add files via upload

--- mafiaRoles/scripts.js
+++ mafiaRoles/scripts.js
@@ -1,65 +1,70 @@
-let generatePackRolesBtn = document.querySelector("#generatePackRoles");
-let roles = document.querySelectorAll(".getValueSelectedRole");
-let rolesCount = roles.length;
-let arrayRoles = [];
-let errorsCheck = false;
+//Переменные
+let generatePackRolesBtn = document.querySelector("#generatePackRoles"); //Получает кнопку "Сгенерировать колоду"
+let roles = document.querySelectorAll(".getValueSelectedRole"); //Получает массив обьектов с ролями и их количеством
+let rolesCount = roles.length; //Получает количество всех возможных ролей для генерации
+let arrayRoles = []; //Создание пустого массива, в котором будет хранится пак выбранных ролей 
+let errorsCheck = false; //Проверка на наличие ошибок
 
-generatePackRolesBtn.addEventListener("click", () => {
-    errorsCheck = false;
-    arrayRoles = [];
-    for (let i = 0; i < rolesCount; i++) {
+generatePackRolesBtn.addEventListener("click", () => { //Отслеживание клика на кнопку генерации ролей
+    errorsCheck = false; //Обновить переменную ошибок к дефолту
+    arrayRoles = []; //Очистить пак ролей
+    for (let i = 0; i < rolesCount; i++) { //Цикл получения всех ролей в массив arrayRoles (С уже выбраным количеством каждой роли)
         for (let j = 0; j < roles[i].value; j++) {
-            arrayRoles.push(roles[i].dataset.role);
-            console.log(arrayRoles[i]);
+            arrayRoles.push(roles[i].dataset.role); //Загрузка очередного элемента в массив пака ролей
         }
     }
-    shuffleTheDeck();
-    if (arrayRoles.length == 0) {
+    shuffleTheDeck(); //Функция случайного перемешивания ролей в массиве
+    if (arrayRoles.length == 0) { //Проверка ошибки на то, если в паке нет выбранных ролей
         alert("Ошибка, вы не выбрали ни одной карты для игры");
-        errorsCheck = false;
-    } else {
+        errorsCheck = false; //Сбросить к дефолту переменную наличия ошибок
+    } else {//Если ошибок не обнаружено
         alert("Колода сгенерирована, можете начинать раздачу ролей");
         errorsCheck = true;
-        endOfDistributionFlag = false;
-        getRoleBtn.innerHTML = "Нажмите для получения роли";
+        endOfDistributionFlag = false; // Переменная-флаг конца показа ролей на экране
+        getRoleBtn.innerHTML = "Нажмите для получения роли"; //Переменная вывода роли на экран
     }
 });
 
-function shuffleTheDeck() {
-    for (let i = 0; i < 100; i++) {
-        arrayRoles.sort(() => Math.random() - 0.5);
+function shuffleTheDeck() { //Функция случайного перемешивания пака в массиве 
+    for (let i = 0; i < 100; i++) { //Цикл перемешивания
+        arrayRoles.sort(() => Math.random() - 0.5); //Случайная сортировка
     }
 };
 
-let getRoleBtn = document.getElementById("role");
-let counter = 0;
-let endOfDistributionFlag = false;
+let getRoleBtn = document.getElementById("role"); //Получить обьект роли, чтобы после получить дата-атрибуты конкретно каждой роли
+let counterVisibleRolesScreen = 0; //Счётчик уже показанных на экране ролей
+let endOfDistributionFlag = false; //Флаг конца показа ролей
+let roleShowTime = 3000; //Время показа роли на экране
 
+function changeTimeViewRoleToScreen(timeInputValue){//Функция смены времени показа роли на экране
+    roleShowTime = timeInputValue * 1000;
+    document.getElementById("timeViewRoleToScreenValue").innerHTML = "Секунд: " + timeInputValue;
+}
 
-role.addEventListener("click", () => {
-    if(!endOfDistributionFlag){
-        if (errorsCheck) {
-            getRoleBtn.innerHTML = "Вы: " + arrayRoles[counter];
-            setTimeout(() => {
-                getRoleBtn.innerHTML = "Нажмите для получения роли";
-            }, 3000);
-            if (counter + 2 <= arrayRoles.length) {
-                counter++;
-            } else {
-                counter = 0;
-                endOfDistributionFlag = true;
+role.addEventListener("click", () => { //Событие клика для показа очережной роли на экране
+    if(!endOfDistributionFlag){ //Если конец показа не достигнут
+        if (errorsCheck) { //Если нет ошибок
+            getRoleBtn.innerHTML = "Вы: " + arrayRoles[counterVisibleRolesScreen]; //Показать очередную роль на экране
+            setTimeout(() => { // Таймер показа роли на экране на определённое время
+                getRoleBtn.innerHTML = "Нажмите для получения роли"; //Скрыть роль с экрана (Поставить "заглушку через время после показа")
+            }, roleShowTime);
+            if (counterVisibleRolesScreen + 2 <= arrayRoles.length) {//Проверка на конец выдачи пака
+                counterVisibleRolesScreen++;//Если не конец, добавить 1 к счётчику показа ролей
+            } else {//Если конец
+                counterVisibleRolesScreen = 0;//Обнулить счётчик показа ролей
+                endOfDistributionFlag = true;//Поставить флаг конца показа в true
             }
-        } else {
-            alert("Ошибка. Колода пуста.");
+        } else {//Если возники ошибки 
+            alert("Ошибка. Колода пуста или что-то пошло не так :)");
         }
-    }else{
+    }else{//Если флаг конца показа в true
         getRoleBtn.innerHTML = "Раздача законечена. Сгенерируйте новую колоду.";
     }
 });
 
 
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+function getRandomInt(min, max) { // Функция генерации случайного числа в ограниченом диапазоне
+    return Math.floor(Math.random() * (max - min + 1) + min); //Вернуть из функции случайное число
 }
 
 // Раздача ролей логика end
@@ -67,6 +72,6 @@ function getRandomInt(min, max) {
 //Кнопка - как раздать роли?
 
 //Закрыть popup
-function openAndclosePopups(objectPopup){
-    document.querySelector("." + objectPopup.dataset.popups).classList.toggle("hideAndShowPopup");
+function openAndclosePopups(objectPopup){ //Функция показа модальных окон на экране (Вызывается непосредственно из HTML через onclick)
+    document.querySelector("." + objectPopup.dataset.popups).classList.toggle("hideAndShowPopup");//Вставить/удалить класс показа окна на элемент
 }
